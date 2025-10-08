70d0c614fa6fb66c5786ecc94a82e7258116f00f
web96lol <ajay.narang@alumni.unlv.edu>
2025-10-08 04:02:37 -0700

    Delete duplicate-tabs-closer-master directory

--- duplicate-tabs-closer-master/scriptImport.js
+++
@@ -1,5 +0,0 @@
-try {
-  importScripts("helper.js", "tabsInfo.js", "options.js", "urlUtils.js", "badge.js", "worker.js", "messageListener.js", "background.js");
-} catch (e) {
-  console.error(e);
-}
